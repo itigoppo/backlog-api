0517b3ddfbd9026020f1545882cb0981d756760d
itigoppo <itigoppo+github@gmail.com>
2017-11-13 11:38:26 +0900

    空テスト追加

---
+++ tests/TestCase/BacklogTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Itigoppo\Backlog\Test;
+
+use PHPUnit\Framework\TestCase;
+
+class BacklogTest extends TestCase
+{
+    public function setUp()
+    {
+        parent::setUp();
+    }
+
+    protected function tearDown()
+    {
+        parent::tearDown();
+    }
+
+    public function testCreate()
+    {
+        // 空テスト
+        $this->assertTrue(true);
+    }
+}
